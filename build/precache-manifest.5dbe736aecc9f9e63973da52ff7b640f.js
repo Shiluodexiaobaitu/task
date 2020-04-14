dc687b2c6c733ef912e9a7026efbcada25224434
zhangce <2361506665@qq.com>
2020-04-14 13:16:28 +0800

    上传build文件

---
+++ build/precache-manifest.5dbe736aecc9f9e63973da52ff7b640f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2057e5c2053c6b397d7d3aff24866f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0071b55f223064e480b",
+    "url": "/static/css/2.2f7a11e4.chunk.css"
+  },
+  {
+    "revision": "3d964e52b8a107337dd9",
+    "url": "/static/css/main.dc307ea8.chunk.css"
+  },
+  {
+    "revision": "b0071b55f223064e480b",
+    "url": "/static/js/2.91c7c5e0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.91c7c5e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d964e52b8a107337dd9",
+    "url": "/static/js/main.a13a8380.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  }
+]);
